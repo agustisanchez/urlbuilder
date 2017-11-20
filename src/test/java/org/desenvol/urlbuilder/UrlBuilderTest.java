de3f8ca67f3f9aea91ccb4dad3a8fb5ae7c9b493
Agustí Sánchez <agusti.sanchez@gmail.com>
2017-11-20 21:55:48 +0100

    a basic unit test

---
+++ src/test/java/org/desenvol/urlbuilder/UrlBuilderTest.java
@@ -0,0 +1,24 @@
+package org.desenvol.urlbuilder;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Unit tests.
+ * 
+ * @author Agustí Sánchez
+ *
+ */
+@RunWith(JUnit4.class)
+public class UrlBuilderTest {
+
+	@Test
+	public void someBasicTest() {
+		String url = new UrlBuilder("http://expressjava.net").path("test").param("someParam", "1/2")
+				.param("anotherParam", "other").build();
+		Assert.assertEquals("http://expressjava.net/test?someParam=1%2F2&anotherParam=other", url);
+	}
+
+}
