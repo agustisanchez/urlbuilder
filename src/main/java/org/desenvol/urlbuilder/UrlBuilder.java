4ef63ef53a20d9a5a331ee94229ffbf887735800
Agustí Sánchez <agusti.sanchez@gmail.com>
2017-11-20 21:50:36 +0100

    NameValuePair turned into an inner class

--- src/main/java/org/desenvol/urlbuilder/UrlBuilder.java
+++ src/main/java/org/desenvol/urlbuilder/UrlBuilder.java
@@ -150,4 +150,34 @@ public class UrlBuilder {
 
 	}
 
+	/**
+	 * 
+	 * Holds a name / value pair to be used to model query parameters and HTTP
+	 * headers.
+	 * 
+	 * @author Agustí Sánchez
+	 *
+	 */
+	class NameValuePair {
+
+		private String name;
+
+		private String value;
+
+		public NameValuePair(String name, String value) {
+			super();
+			this.name = name;
+			this.value = value;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+	}
+
 }
